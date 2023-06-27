c78bbd2bedabcbb834386a2a5584bdfa9bfa5196
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2023-06-27 18:19:23 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,58 +1,204 @@
-// 02-timer.js
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+// Імпортуємо потрібні бібліотеки
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-const datetimePicker = document.querySelector("#datetime-picker");
-const startBtn = document.querySelector('[data-start]');
-const daysEl = document.querySelector('[data-days]');
-const hoursEl = document.querySelector('[data-hours]');
-const minutesEl = document.querySelector('[data-minutes]');
-const secondsEl = document.querySelector('[data-seconds]');
+// Знаходимо елементи на сторінці.
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+const alertSound = document.querySelector('#alertSound');
 
+// Налаштування гучності звуку
+alertSound.volume = 0.3;
+
+// Знаходимо кнопку start на сторінці
+const startBtn = document.querySelector('button[data-start]');
+
+// Налаштування для вибору дати та часу
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    if (selectedDate < Date.now()) {
-      alert("Please choose a date in the future.");
-      return;
+    targetDate = selectedDates[0];
+    if (targetDate.getTime() < options.defaultDate.getTime()) {
+      alertSound.play();
+      Notiflix.Report.warning(
+        "Упс ! Обрану дату з'їли динозаври...",
+        'Будь ласка, оберіть дату з майбутнього',
+        'Гаразд'
+      );
+      startBtn.classList.remove('valid-date');
+      startBtn.classList.add('invalid-date');
+    } else {
+      startBtn.classList.remove('invalid-date');
+      startBtn.classList.add('valid-date');
     }
-    startBtn.disabled = false;
   },
 };
 
-flatpickr(datetimePicker, options);
+// Використовуємо flatpickr для вибору дати та часу
+flatpickr('input#datetime-picker', options);
 
-startBtn.addEventListener('click', onStartBtnClick);
+// Ініціалізуємо змінні для відстеження цільової дати та таймера
+let targetDate = null;
+let timer = null;
+const interval = 1000;
 
-function onStartBtnClick() {
-  const selectedDate = flatpickr.formatDate(datetimePicker.value, "Y-m-d H:i:S");
-  const countdownDate = new Date(Date.parse(selectedDate)).getTime();
+// Додаємо слухача подій до кнопки start
+startBtn.addEventListener('click', () => {
+  if (targetDate && !timer) { 
+      timer = setInterval(() => {
+          let currentDateInMs = new Date().getTime();
+          let timeDiff = targetDate.getTime() - currentDateInMs;
+          if (timeDiff <= 0) {
+              clearInterval(timer);
+              timer = null; 
+              
+              let reloadIcon = document.querySelector('.reload-icon');
+              if (reloadIcon) {
+                  document.body.removeChild(reloadIcon);
+              }
 
+              return;
+          }
 
-  const updateTimer = setInterval(() => {
-    const currentTime = new Date().getTime();
-    const timeDifference = countdownDate - currentTime;
+          let remainingTime = convertMs(timeDiff);
+          const timeUnits = {
+              days: daysSpan,
+              hours: hoursSpan,
+              minutes: minutesSpan,
+              seconds: secondsSpan,
+          };
+          
+          // Оновлення відображення часу на сторінці
+          Object.keys(timeUnits).forEach((unit) => {
+              timeUnits[unit].textContent = String(remainingTime[unit]).padStart(2, '0');
+          });
+          
+          // Перевірка чи іконка для перезавантаження вже існує
+          if (!document.querySelector('.reload-icon')) {
+            const reloadIcon = document.createElement('div');
+            reloadIcon.classList.add('reload-icon'); 
+            reloadIcon.innerHTML = "&#8635; reset";
 
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+              
+            // Додаємо слухача подій до іконки перезавантаження
+            reloadIcon.addEventListener('click', function() {
+                location.reload();
+            });
+        
+            const container = document.querySelector('.container'); 
+            container.appendChild(reloadIcon); 
+        }
 
-    daysEl.textContent = String(days).padStart(2, "0");
-    hoursEl.textContent = String(hours).padStart(2, "0");
-    minutesEl.textContent = String(minutes).padStart(2, "0");
-    secondsEl.textContent = String(seconds).padStart(2, "0");
+        startBtn.style.display = 'none'
+    
 
-    if (timeDifference < 0) {
-      clearInterval(updateTimer);
-      daysEl.textContent = "00";
-      hoursEl.textContent = "00";
-      minutesEl.textContent = "00";
-      secondsEl.textContent = "00";
-    }
-  }, 1000);
+      }, interval);
+  }
+});
+
+// Функція для перетворення мілісекунд в дні, години, хвилини, та секунди
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ------------------------ BUTTON  VISUAL --------------------------
+const createSVG = (width, height, radius) => {
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+  const rectangle = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'rect'
+  );
+
+  svg.setAttributeNS(
+    'http://www.w3.org/2000/svg',
+    'viewBox',
+    `0 0 ${width} ${height}`
+  );
+
+  rectangle.setAttribute('x', '0');
+  rectangle.setAttribute('y', '0');
+  rectangle.setAttribute('width', '100%');
+  rectangle.setAttribute('height', '100%');
+  rectangle.setAttribute('rx', `${radius}`);
+  rectangle.setAttribute('ry', `${radius}`);
+  rectangle.setAttribute('pathLength', '10');
+
+  svg.appendChild(rectangle);
+
+  return svg;
+};
+
+document.querySelectorAll('.sketch-button').forEach(button => {
+  const style = getComputedStyle(button);
+
+  const lines = document.createElement('div');
+
+  lines.classList.add('lines');
+
+  const groupTop = document.createElement('div');
+  const groupBottom = document.createElement('div');
+
+  const svg = createSVG(
+    button.offsetWidth,
+    button.offsetHeight,
+    parseInt(style.borderRadius, 10)
+  );
+
+  groupTop.appendChild(svg);
+  groupTop.appendChild(svg.cloneNode(true));
+  groupTop.appendChild(svg.cloneNode(true));
+  groupTop.appendChild(svg.cloneNode(true));
+
+  groupBottom.appendChild(svg.cloneNode(true));
+  groupBottom.appendChild(svg.cloneNode(true));
+  groupBottom.appendChild(svg.cloneNode(true));
+  groupBottom.appendChild(svg.cloneNode(true));
+
+  lines.appendChild(groupTop);
+  lines.appendChild(groupBottom);
+
+  button.appendChild(lines);
+
+  button.addEventListener('pointerenter', () => {
+    button.classList.add('start');
+  });
+
+  svg.addEventListener('animationend', () => {
+    button.classList.remove('start');
+  });
+});
